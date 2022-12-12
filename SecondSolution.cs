39a2fed17616b902ebbe1d334cdcae948077a193
DenisVasilev05 <denisvasilev05@gmail.com>
2022-12-12 18:44:38 +0200

    Add CheckForParentheses method
    Add a function that checks if the equation has any parentheses that need to be expanded before solving the problem.

--- SecondSolution.cs
+++ SecondSolution.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace MySolution
 {
@@ -12,10 +13,9 @@ namespace MySolution
         public static async Task Main()
         {
             string lettersFilter = "[a-z]";
+            //Input in format: a*x + b - c = d*x + e OR a*x + b - c
             string[] inputExpressions = await File
                 .ReadAllLinesAsync("Your input path");
-            //Input in format: a*x + b - c = d*x + e OR a*x + b - c
-
             for (int i = 0; i < inputExpressions.Length; i++)
             {
                 string formattedExpression = Regex.Replace(inputExpressions[i], lettersFilter, "x");
@@ -30,24 +30,37 @@ namespace MySolution
         {
             if (inputExpression.Contains('=')) //Check if the problem is an equation or a simpler math problem
             {
+                string leftSide = inputExpression.Split('=', StringSplitOptions.RemoveEmptyEntries)[0]!.Trim();
+                string rightSide = inputExpression.Split('=', StringSplitOptions.RemoveEmptyEntries)[1]!.Trim();
+                //Check for parentheses: 9 - 2*(x - 5) = x + 10 --> 19 - 2*x = 10 + x
+                leftSide = CheckForParentheses(leftSide);
+                rightSide = CheckForParentheses(rightSide);
                 //Normalize the equation: a*x + b - c = d*x + e --> a*x - d*x = e - b + c (unknowns on the left, numbers on the right)
-                inputExpression = NormalizeEquation(inputExpression.Split('=', StringSplitOptions.RemoveEmptyEntries)[0]!.Trim(),
-                                                    inputExpression.Split('=', StringSplitOptions.RemoveEmptyEntries)[1]!.Trim());
-                //Simplify left output as much as possible
                 string leftOutput = SymbolicExpression
-                    .Parse(inputExpression
+                    .Parse(NormalizeEquation(leftSide, rightSide) //Simplify left output as much as possible
                     .Split('=', StringSplitOptions.RemoveEmptyEntries)[0]
                     .Trim()).ToString();
-                //Simplify right output as much as possible
-                string rightOutput = SymbolicExpression.Parse(inputExpression
+                string rightOutput = SymbolicExpression
+                    .Parse(NormalizeEquation(leftSide, rightSide) //Simplify right output as much as possible
                     .Split('=', StringSplitOptions.RemoveEmptyEntries)[1]
                     .Trim()).ToString();
                 return string.Concat(leftOutput, " = ", rightOutput); //Concat both sides to form the final equation for the solution
             }
             else
             {
-                return SymbolicExpression.Parse(inputExpression).ToString(); //Solve the output as much as possible
+                return SymbolicExpression.Parse(CheckForParentheses(inputExpression)).ToString(); //Solve the output as much as possible
             }
+        }
+        //Method to check if either side of the equation has any parentheses to be expaned before solving
+        private static string CheckForParentheses(string side)
+        {
+            if (side.Contains('(') && side.Contains(')'))
+            {
+                Expression expression = Infix.ParseOrThrow(side);
+                Expression expanded = Algebraic.Expand(expression);
+                side = Infix.Format(expanded);
+            }
+            return side;
         }
         //Method for normalizing equations if need be
         private static string NormalizeEquation(string leftSide, string rightSide)
@@ -132,7 +145,7 @@ namespace MySolution
             }
             leftSideNumbers = RemoveRemainingNumbersAndSigns(leftSideNumbers, leftSideNumbersToBeRemoved);
             rightSideNumbers = RemoveRemainingNumbersAndSigns(rightSideNumbers, rightSideNumbersToBeRemoved);
-            return string.Concat(string.Join(" ", leftSideNumbers), " = ", string.Join(" ", rightSideNumbers)); //Returning the final equation for the solution
+            return string.Concat(string.Join(" ", leftSideNumbers, " = ", string.Join(" ", rightSideNumbers))); //Returning the final equation for the solution
         }
         //Method for removal of unnecessary numbers and signs (sort of garbage collection for both sides)
         private static List<string> RemoveRemainingNumbersAndSigns(List<string> currentSideNumbers, List<string> currentSideNumbersToBeRemoved)
